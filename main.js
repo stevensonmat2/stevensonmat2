62c7998dfc0793089944d259cc2f03fdd03a489e
stevensonmat2 <stevensonmat2@gmail.com>
2023-03-14 22:26:18 -0700

    Improve accessibility, refactor js - fixed accesibility errors - refactored js - added images and links for projects

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+const projects = [
+  {
+    src: "img/game-of-life.png",
+    alt: "A screenshot of the game of life",
+    title: "Game of Life",
+    href: "https://github.com/stevensonmat2/game_of_life",
+  },
+  {
+    src: "img/mobile-phone-inbox.jpg",
+    alt: "A hand holding a mobile phone showing an email inbox",
+    title: "Mass-mailer Script",
+    href: "https://github.com/stevensonmat2/mass_mailer",
+  },
+  {
+    src: "img/chess.jpg",
+    alt: "A child with glasses moving chess pieces on a chess board",
+    title: "Chess-playing AI",
+    href: "https://github.com/stevensonmat2/ai_group_project",
+  },
+];
+var currentProject = 0;
+
+document.querySelector("#right-chevron").addEventListener("click", shiftRight);
+document.querySelector("#left-chevron").addEventListener("click", shiftLeft);
+
+updateProjectsData();
+
+function updateProjectsData() {
+  var projectImage = document.getElementById("project-image");
+  var projectLink = document.getElementById("project-link");
+  var projectTitle = document.getElementById("project-title");
+  projectImage.src = projects[currentProject].src;
+  projectImage.alt = projects[currentProject].alt;
+  projectImage.title = projects[currentProject].title;
+  projectLink.href = projects[currentProject].href;
+  projectTitle.innerText = projects[currentProject].title;
+}
+
+function shiftRight() {
+  if (currentProject == projects.length - 1) {
+    currentProject = 0;
+  } else {
+    currentProject += 1;
+  }
+  updateProjectsData();
+}
+
+function shiftLeft() {
+  if (currentProject == 0) {
+    currentProject = projects.length - 1;
+  } else {
+    currentProject -= 1;
+  }
+  updateProjectsData();
+}
+
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   var screenHeight = $(window).height();
@@ -8,65 +64,3 @@ $(window).scroll(function () {
     $(".nav-list").removeClass("nav-colored");
   }
 });
-
-const projects = [
-  {
-    src: "img/orange_cycle.png",
-    alt: "a screenshot of the game of life",
-    title: "Game of Life screenshot",
-    href: "https://github.com/stevensonmat2/game_of_life",
-  },
-  {
-    src: "img/orange_gear.png",
-    alt: "gear",
-    title: "test1",
-    href: "",
-  },
-  {
-    src: "img/orange_pencil.png",
-    alt: "pencil",
-    title: "test2",
-    href: "",
-  },
-];
-
-var currentProject = 0;
-
-document
-  .querySelector("#right-chevron")
-  .addEventListener("click", shiftRight, false);
-document
-  .querySelector("#left-chevron")
-  .addEventListener("click", shiftLeft, false);
-
-function shiftRight() {
-  var projectImage = document.getElementById("project-image");
-  var projectLink = document.getElementById("project-link");
-
-  if (currentProject == projects.length - 1) {
-    currentProject = 0;
-  } else {
-    currentProject += 1;
-  }
-
-  projectImage.src = projects[currentProject].src;
-  projectImage.alt = projects[currentProject].alt;
-  projectImage.title = projects[currentProject];
-  projectLink.href = projects[currentProject].href;
-}
-
-function shiftLeft() {
-  var projectImage = document.getElementById("project-image");
-  var projectLink = document.getElementById("project-link");
-
-  if (currentProject == 0) {
-    currentProject = projects.length - 1;
-  } else {
-    currentProject -= 1;
-  }
-
-  projectImage.src = projects[currentProject].src;
-  projectImage.alt = projects[currentProject].alt;
-  projectImage.title = projects[currentProject];
-  projectLink.href = projects[currentProject].href;
-}
