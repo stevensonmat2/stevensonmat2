4e70b435503d48c039108ece5d8cbd81c604a532
stevensonmat2 <stevensonmat2@gmail.com>
2023-03-17 14:38:12 -0700

    remove color change from navbar

--- main.js
+++ main.js
@@ -61,14 +61,3 @@ function shiftLeft() {
   }
   updateProjectsData();
 }
-
-$(window).scroll(function () {
-  var scroll = $(window).scrollTop();
-  var screenHeight = $(window).height();
-
-  if (scroll >= screenHeight) {
-    $(".nav-list").addClass("nav-colored");
-  } else {
-    $(".nav-list").removeClass("nav-colored");
-  }
-});
