7041ad1e415b8928d6eac75301efe15d785ff714
stevensonmat2 <stevensonmat2@gmail.com>
2023-03-17 14:15:43 -0700

    Fix image and navbar, add resume - made images all same dimensions - add background color on hover for navbar - add resume section with pdf download

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ const projects = [
     alt: "A screenshot of the game of life",
     title: "Game of Life",
     href: "https://github.com/stevensonmat2/game_of_life",
+    description:
+      "This is my implementation of Conway's Game of Life, a cellular automata simulation.",
   },
   {
-    src: "./img/mobile-phone-inbox.jpg",
+    src: "./img/mobile-phone-inbox.png",
     alt: "A hand holding a mobile phone showing an email inbox",
     title: "Mass-mailer Script",
     href: "https://github.com/stevensonmat2/mass_mailer",
+    description:
+      "I developed this script to help with sending personalized emails to potential volunteers.",
   },
   {
-    src: "./img/chess.jpg",
+    src: "./img/chess.png",
     alt: "A child with glasses moving chess pieces on a chess board",
     title: "Chess-playing AI",
     href: "https://github.com/stevensonmat2/ai_group_project",
+    description:
+      "This project implements basic AI techniques to create a chess-playing program.",
   },
 ];
 var currentProject = 0;
@@ -29,11 +35,13 @@ function updateProjectsData() {
   var projectImage = document.getElementById("project-image");
   var projectLink = document.getElementById("project-link");
   var projectTitle = document.getElementById("project-title");
+  var projectDescription = document.getElementById("project-description");
   projectImage.src = projects[currentProject].src;
   projectImage.alt = projects[currentProject].alt;
   projectImage.title = projects[currentProject].title;
   projectLink.href = projects[currentProject].href;
   projectTitle.innerText = projects[currentProject].title;
+  projectDescription.innerText = projects[currentProject].description;
 }
 
 function shiftRight() {
